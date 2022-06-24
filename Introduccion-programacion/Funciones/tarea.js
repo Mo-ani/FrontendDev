8aab6833f7301f534d4591a7442151d7030c0b94
Luis <peterpd3w@gmail.com>
2022-06-24 12:19:17 -0500

    Finish

--- Introduccion-programacion/Funciones/tarea.js
+++ Introduccion-programacion/Funciones/tarea.js
@@ -1,6 +1,3 @@
-
-
-
 /*
 Primera parte:
 Crear una función con tres parámetros que sean números que se suman entre sí.
@@ -11,8 +8,6 @@ function suma(p1,p2,p3){
     console.log(p1+p2+p3);
 }
 
-
-
 /*
 Crear una clase coche.
 
@@ -20,7 +15,6 @@ Dentro de la clase coche, una variable numérica que almacene el número de puer
 Una función que incremente el número de puertas que tiene el coche.
 Crear un objeto miCoche en el main y añadirle una puerta.
 Mostrar el número de puertas que tiene el objeto.
-
 */ 
 
 class Coche{
@@ -38,7 +32,7 @@ const main = function(){
     const resultado = suma(1,4,3);
 
     let miCoche = new Coche(1);
-    
+
     miCoche.incrementar()//'Este coche esta aumenta sus puertas a 4'
     miCoche.incrementar()//'Este coche esta aumenta sus puertas a 5'
     miCoche.incrementar()//'Este coche esta aumenta sus puertas a 6'
